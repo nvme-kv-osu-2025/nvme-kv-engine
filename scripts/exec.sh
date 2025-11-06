11ff639d1e9192170d7910215364a8cffbdd87bf
Cody Strehlow <codystray@gmail.com>
2025-11-06 15:13:13 -0800

    feat_17: fixing build script paths

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,4 +1,5 @@
-./scripts/build_docker.sh
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+. "$SCRIPT_DIR"/build_docker.sh
 
 # Enter container
-docker exec -it kvssd-container bash ./build_with_test.sh
+docker exec -it kvssd-container bash ./scripts/build_with_test.sh
