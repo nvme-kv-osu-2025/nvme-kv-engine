609e020401c4a2e74a67dadb0fd64f8efd97c157
lilgangus <tangcharles29@gmail.com>
2025-11-05 23:27:36 -0800

    feat_14: intial project setup, openMPDK/KVSSD: e4739e861e3115fe0c3a6757f0cf535aa5b7838b, spdk:f288c545b6e9b9139b9e8671e8c7b3bf6fefa83d, nvmevirt:c8ef01d52d54f0b49239a5b4a8e15a31b353597f

---
+++ scripts/build_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+IMAGE_NAME="kvssd-emulator"
+CONTAINER_NAME="kvssd-container"
+
+PROJECT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+echo "Project directory is: $PROJECT_DIR"
+
+DOCKERFILE_DIR="$PROJECT_DIR/docker/dev"
+
+if [ -z "$(docker images -q "$IMAGE_NAME")" ]; then
+    echo "Docker image '$IMAGE_NAME' not found, building..."
+    docker build -t "$IMAGE_NAME" "$DOCKERFILE_DIR"
+else
+    echo "Docker image '$IMAGE_NAME' already exists, no new build"
+fi
+
+if [ "$(docker ps -aq -f name="^${CONTAINER_NAME}$")" ]; then
+    echo "Container '$CONTAINER_NAME' already exists. removing..."
+    docker rm -f "$CONTAINER_NAME"
+fi
+
+# container in detached, mount to project parent directory
+docker run -d \
+  --name "$CONTAINER_NAME" \
+  -v "$PROJECT_DIR:/user" \
+  "$IMAGE_NAME" \
+  tail -f /dev/null
+
+echo "Container '$CONTAINER_NAME' is running with image '$IMAGE_NAME'."
