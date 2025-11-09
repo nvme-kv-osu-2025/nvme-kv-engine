dca4d4b3ec3df727102bb9725c7266521cc3863b
lilgangus <tangcharles29@gmail.com>
2025-11-09 00:47:00 -0800

    feat_27: Added clang formating script

---
+++ scripts/clang_format.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if NOEDIT flag is passed
+NOEDIT=false
+if [[ "$1" == "NOEDIT" ]]; then
+  NOEDIT=true
+fi
+
+for dir in src include examples; do
+  if $NOEDIT; then
+    # Just check formatting, don't modify files
+    find "$dir" -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -style="{UseTab: ForIndentation}" --dry-run --Werror {} +
+  else
+    # Actually format files
+    find "$dir" -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i -style="{UseTab: ForIndentation}" {} +
+  fi
+done
