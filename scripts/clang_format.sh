5667ac9cfddbd8c3c2d63ba5bb994ab2a8a49986
lilgangus <tangcharles29@gmail.com>
2025-11-09 18:56:01 -0800

    feat_27: Small nit fixes

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -9,12 +9,12 @@ fi
 
 for dir in src include examples; do
     if $NOEDIT; then
-    # Just check formatting, don't modify files
+        # Just check formatting, don't modify files
         for file in $(find "$dir" -type f \( -name "*.cpp" -o -name "*.h" \)); do
             clang-format --dry-run --Werror "$file"
         done
     else
-    # Actually format files
+        # Actually format files
         find "$dir" -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i {} +
     fi
 done
--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# this script execute the arguments into the container
+# this script executes the arguments into the container
+CONTAINER_NAME="kvssd-container"
 
-DOCKER=false
-
+# docker flag passed in for CI to not execute with -t flag, cannot add t flag because no TTY in CI
 if [[ "$1" == "DOCKER" ]]; then
     shift
     docker exec -i "$CONTAINER_NAME" bash -c "$*"
 else 
-    CONTAINER_NAME="kvssd-container"
     if [ "$#" -eq 0 ]; then
         docker exec -it "$CONTAINER_NAME" bash
     fi
