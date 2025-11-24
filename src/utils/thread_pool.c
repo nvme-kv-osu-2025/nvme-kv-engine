c5ba283e3038b7bbf23ad254e6b1a04a7387b36d
lilgangus <tangcharles29@gmail.com>
2025-11-23 23:28:20 -0800

    feat/thread_pooling_9: Intial feature and testing

--- src/utils/thread_pool.c
+++ src/utils/thread_pool.c
@@ -6,23 +6,38 @@
 #include "kv_engine_internal.h"
 #include <stdlib.h>
 
+thread_pool_thread_t* thread_pool_thread_create_array(uint32_t num_threads) {
+    thread_pool_thread_t* threads = (thread_pool_thread_t*)malloc(
+        num_threads * sizeof(thread_pool_thread_t));
+    if (!threads) {
+        return NULL;
+    }
+
+    for (uint32_t i = 0; i < num_threads; i++) {
+        threads[i].id = i;
+        threads[i].busy = 0;
+        threads[i].current_task = NULL;
+    }
+
+    return threads;
+}
+
 thread_pool_t* thread_pool_create(uint32_t num_threads) {
     thread_pool_t* pool = (thread_pool_t*)malloc(sizeof(thread_pool_t));
     if (!pool) {
         return NULL;
     }
 
-    pool->threads = (pthread_t*)calloc(num_threads, sizeof(pthread_t));
-    if (!pool->threads) {
+    pool->total_threads = num_threads;
+    pool->free_threads_count = num_threads;
+    pool->busy_threads_count = 0;
+    pool->shutdown = 0;
+    pool->busy_threads = NULL;
+    pool->free_threads = thread_pool_thread_create_array(num_threads);
+    if (!pool->free_threads) {
         free(pool);
         return NULL;
     }
-
-    pool->num_threads = num_threads;
-    pool->shutdown = 0;
-
-    /* TODO: Initialize work queue */
-    /* TODO: Create worker threads */
 
     return pool;
 }
@@ -39,19 +54,40 @@ int thread_pool_submit(thread_pool_t* pool, void* (*func)(void*), void* arg) {
     return 0;
 }
 
+int thread_pool_thread_destroy(thread_pool_thread_t* thread) {
+    if (!thread) {
+        return -1
+    }
+
+    thread->async_context_t = NULL;
+
+    free(thread);
+    return 0;
+}
+
 int thread_pool_destroy(thread_pool_t* pool) {
     if (!pool) {
-        return;
+        return -1;
     }
+
+    // stop the busy threads
+    for (uint32_t i = 0; i < pool->busy_threads_count; i++) {
+        if (thread_pool_thread_destroy(&pool->busy_threads[i]) != 0) {
+            return -1;
+        }
+    }
+
+    for (uint32_t i = 0; i < pool->free_threads_count; i++) {
+        if (thread_pool_thread_destroy(&pool->free_threads[i]) != 0) {
+            return -1;
+        }
+    }
+
+    free(pool->busy_threads);
+    free(pool->free_threads);
+    free(pool);
 
     pool->shutdown = 1;
+    return 0;
 
-    /* TODO: Signal all threads to shutdown */
-    /* TODO: Join all threads */
-
-    if (pool->threads) {
-        free(pool->threads);
-    }
-
-    free(pool);
 }
