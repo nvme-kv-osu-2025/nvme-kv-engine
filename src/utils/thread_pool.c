9c31c8ae61efb8fc6c8b59de87898e17991f2a2d
lilgangus <tangcharles29@gmail.com>
2026-03-05 23:56:34 -0800

    Rebase fix

--- src/utils/thread_pool.c
+++ src/utils/thread_pool.c
@@ -10,56 +10,6 @@
 #include "kv_engine_internal.h"
 #include <stdlib.h>
 
-<<<<<<< HEAD
-thread_pool_t *thread_pool_create(uint32_t num_threads) {
-  // thread_pool_t* pool = (thread_pool_t*)malloc(sizeof(thread_pool_t));
-  // if (!pool) {
-  //     return NULL;
-  // }
-
-  // pool->threads = (pthread_t*)calloc(num_threads, sizeof(pthread_t));
-  // if (!pool->threads) {
-  //     free(pool);
-  //     return NULL;
-  // }
-
-  // pool->num_threads = num_threads;
-  // pool->shutdown = 0;
-
-  // /* TODO: Initialize work queue */
-  // /* TODO: Create worker threads */
-
-  // return pool;
-}
-
-int thread_pool_submit(thread_pool_t *pool, void *(*func)(void *), void *arg) {
-  // if (!pool || !func) {
-  //     return -1;
-  // }
-
-  // /* TODO: Add work to queue */
-  // /* For now, just execute synchronously */
-  // func(arg);
-
-  // return 0;
-}
-
-void thread_pool_destroy(thread_pool_t *pool) {
-  // if (!pool) {
-  //     return;
-  // }
-
-  // pool->shutdown = 1;
-
-  // /* TODO: Signal all threads to shutdown */
-  // /* TODO: Join all threads */
-
-  // if (pool->threads) {
-  //     free(pool->threads);
-  // }
-
-  // free(pool);
-=======
 /* Worker thread entry point */
 static void *thread_pool_worker(void *arg) {
   thread_pool_t *pool = (thread_pool_t *)arg;
@@ -235,5 +185,4 @@ void thread_pool_destroy(thread_pool_t *pool) {
   pthread_cond_destroy(&pool->queue_not_full);
   free(pool->threads);
   free(pool);
->>>>>>> 215e214 (Implement thread pool and async operations)
 }
